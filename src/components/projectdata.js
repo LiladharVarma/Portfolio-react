d8d4fbcb13af71f31f6dd7fe7bf0f5e9c10eaddf
Liladhar Varma <liladharvarm29@gmail.com>
2024-01-16 12:56:54 +0530

    pinterest project added

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -4,6 +4,8 @@ import weatherAppLogo from "../images/w.png";
 
 import formLogo from "../images/formImage.png";
 
+import pinImage from "../images/pinImage.png"
+
 const projectdata = [
   {
     id: 0,
@@ -15,6 +17,14 @@ const projectdata = [
   },
   {
     id: 1,
+    img: pinImage,
+    heading: "Pinterest UI Clone",
+    content:
+      "Created a Pinterest UI clone using React, emphasizing a captivating and user-friendly design. Implemented React's component-based architecture and seamlessly integrated API calls for dynamic image retrieval, showcasing proficiency in React and adept handling of data presentation. ",
+    tryLink: "https://pinterest-clone-react-1292.netlify.app/",
+  },
+  {
+    id: 2,
     img: currencyconverter,
     heading: "Currency Converter",
     content:
@@ -22,13 +32,14 @@ const projectdata = [
     tryLink: "https://liladharvarma.github.io/CurrencyConverterJS/",
   },
   {
-    id: 2,
+    id: 3,
     img: formLogo,
     heading: "Login Form",
     content:
       "Created Basic login form using React and also learned some react concepts by doing.Created components for different fields ",
     tryLink: "https://liladharvarma.github.io/CurrencyConverterJS/",
   },
+  
 ];
 
 export default projectdata;
